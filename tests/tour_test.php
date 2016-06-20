bebe57c805e80c7e4ba48332b8e63790cfbf9da3
Andrew Nicols <andrew@nicols.co.uk>
2016-06-20 21:34:01 +0800

    Correct variable name in tour_test (Fixes #1)

--- tests/tour_test.php
+++ tests/tour_test.php
@@ -918,7 +918,7 @@ class tour_testcase extends advanced_testcase {
      *
      * @dataProvider sortorder_first_last_provider
      */
-    public function test_is_last_tour_calculated($sortorder, $islast, $total, $islast) {
+    public function test_is_last_tour_calculated($sortorder, $isfirst, $total, $islast) {
         $tour = new \local_usertours\tour();
 
         $rc = new \ReflectionClass('\local_usertours\tour');
